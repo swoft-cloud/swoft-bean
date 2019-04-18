553504fc99fcfcc84261e7dd7e3cb121511fc9bc
stelin <794774870@qq.com>
2019-04-18 22:14:04 +0800

    Added config unit

--- test/unit/BeanTest.php
+++ test/unit/BeanTest.php
@@ -6,6 +6,11 @@ namespace SwoftTest\Bean\Unit;
 
 use PHPUnit\Framework\TestCase;
 
+/**
+ * Class BeanTest
+ *
+ * @since 2.0
+ */
 class BeanTest extends TestCase
 {
     public function testSingle()
